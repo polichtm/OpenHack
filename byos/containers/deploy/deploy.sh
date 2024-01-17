609f7af0fe0e80476059f16cb7cd1c1a580c83c9
Michael Washam <mwasham@microsoft.com>
2024-01-17 15:24:22 -0500

    Update deploy.sh

--- byos/containers/deploy/deploy.sh
+++ byos/containers/deploy/deploy.sh
@@ -205,7 +205,7 @@ then
 
     # Create Azure SQL DB
     echo "Creating Azure SQL DB..."
-    az sql db create -g $teamRG -s $sqlServerName -n $sqlDBName --bsr local
+    az sql db create -g $teamRG -s $sqlServerName -n $sqlDBName --bsr local --service-objective S0
 
 else
     echo "Failed to create SQL Server."
